5f759719fe52cb6f1fbda4c7cd2525b29b3faeda
wangjian <36315610@qq.com>
2022-04-22 09:54:51 +0800

    first commit

---
+++ junit4/src/main/java/com/hg/auto/junit4/Junit4_OneTest.java
@@ -0,0 +1,42 @@
+package com.hg.auto.junit4;
+
+import org.junit.*;
+
+public class Junit4_OneTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Junit4_OneTest.beforeClass");
+    }
+
+    @Before
+    public void before() {
+        System.out.println("Junit4_OneTest.before");
+    }
+
+    @Test
+    public void testJunit01() {
+        System.out.println("Junit4_OneTest.testJunit01");
+    }
+
+    @Test
+    public void testJunit02() {
+        System.out.println("Junit4_OneTest.testJunit02");
+    }
+
+    @Test
+    @Ignore
+    public void testJunit03() {
+        System.out.println("Junit4_OneTest.testJunit03");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Junit4_OneTest.after");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Junit4_OneTest.afterClass");
+    }
+}
